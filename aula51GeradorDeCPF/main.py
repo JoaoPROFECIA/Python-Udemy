bb4dda284eb5e3d037bc93a6fa0035a186d6be0b
Joao Victor <victorjv3288@gmail.com>
2022-07-13 16:35:13 -0300

    Gerador de CPF

--- aula51GeradorDeCPF/main.py
+++ aula51GeradorDeCPF/main.py
@@ -5,4 +5,26 @@ Gerador de CPF
 from random import randint
 
 numero = str(randint(100000000, 999999999))
-print(numero)
+
+novo_cpf = numero                   
+reverso = 10                            # Contador reverso
+total = 0                             
+
+# Loop do CPF
+for index in range(19):
+    if index > 8:                       # Primeiro índice vai de 0 a 9
+        index -= 9                      # São os 9 primeiros dígitos do CPF
+
+    total += int(novo_cpf[index]) * reverso  # SValor total da multiplicação
+    
+    reverso -= 1                        # Decrementa o contador reverso
+    if reverso < 2:
+        reverso = 11
+        diferenca = 11 - (total % 11)
+
+        if diferenca > 9:               # Se o dígito for maior que 9 o valor é 0
+            diferenca = 0
+        total = 0                       # Zera o total
+        novo_cpf += str(diferenca)      # Adiciona o dígito ao CPF
+
+print(f'O novo CPF gerado é: {novo_cpf}')
